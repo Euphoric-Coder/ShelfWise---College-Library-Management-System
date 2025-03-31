f38e183d6fc79688451e0daa0be0ea4f7756600e
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2025-04-01 00:06:52 +0530

    Add initial layout and page components; remove unused files

---
+++ app/(route)/page.js
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Button className="rounded-3xl">Click me</Button>
+    </div>
+  );
+}
+
+export default Home
