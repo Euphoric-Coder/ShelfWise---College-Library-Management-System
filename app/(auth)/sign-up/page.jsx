b5bbac9dc6cd1772b11114c2a32c3090aef6ea5c
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2025-08-23 13:37:52 +0530

    Refactor sign-up page to utilize Auth component with validation schema and default values

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import Auth from "@/components/Auth";
+// import { signUp } from "@/lib/actions/auth";
+import { signUpSchema } from "@/lib/signInSchema";
 
-export default page
+const Page = () => (
+  <Auth
+    type="SIGN_UP"
+    schema={signUpSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+      fullName: "",
+      universityId: 0,
+      universityCard: "",
+    }}
+    // onSubmit={signUp}
+  />
+);
+
+export default Page;
