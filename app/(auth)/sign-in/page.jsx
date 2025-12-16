8e091657402cdde1023fe8ade91e29ac19e225c7
Euphoric-Coder <deydsagnik48@gmail.com>
2025-12-17 02:19:18 +0530

    Add Suspense on the signin page

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -1,20 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import Auth from "@/components/Auth";
 import { signInSchema } from "@/lib/dataValidation";
 import { signInWithCredentials } from "@/lib/Authenticate";
 
 const Page = () => (
-  <Auth
-    type="SIGN_IN"
-    schema={signInSchema}
-    defaultValues={{
-      email: "",
-      password: "",
-    }}
-    onSubmit={signInWithCredentials}
-  />
+  <Suspense fallback={<></>}>
+    <Auth
+      type="SIGN_IN"
+      schema={signInSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+      }}
+      onSubmit={signInWithCredentials}
+    />
+  </Suspense>
 );
 
 export default Page;
